ff7e9a9a46cebadb7286886a3e22b0a8684c9af8
vitalii.mazurenko <vitalii.mazurenko@qsc.com>
2018-11-07 11:22:42 +0200

    setup redux-zero

--- preview-src/index.tsx
+++ preview-src/index.tsx
@@ -1,5 +1,12 @@
 import { h, render } from 'preact';
+import * as createStore from 'redux-zero';
+import { Provider } from 'redux-zero/preact';
 
-const Main = () => <div>Hello World</div>;
+const store = (createStore as any)({
+  profile: {},
+  items: []
+});
+
+const Main = () =>  <Provider store={store}><div>Hello World</div></Provider>;
 
 render(<Main />, document.querySelector('#root') as HTMLElement);
