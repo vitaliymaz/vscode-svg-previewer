788648890d28363b6403e6338bca3842966324e8
vitalii.mazurenko <vitalii.mazurenko@qsc.com>
2018-12-12 15:18:45 +0200

    increase zoom spped by toolbar buttons

--- preview-src/containers/ToolbarContainer.tsx
+++ preview-src/containers/ToolbarContainer.tsx
@@ -4,6 +4,8 @@ import Toolbar from '../components/Toolbar';
 import { actions, IState, ISource } from '../store';
 import { getByteCountByContent, humanFileSize } from '../utils/fileSize';
 
+const SCALE_STEP = 0.4;
+
 interface ToolbarContainerProps {
     changeBackground: Function;
     zoomIn: Function;
@@ -43,12 +45,20 @@ class ToolbarContainer extends Component<ToolbarContainerProps, ToolbarContainer
         this.setState({ activeBtn: '' });
     }
 
+    zoomIn = () => {
+        this.props.zoomIn(SCALE_STEP);
+    }
+
+    zoomOut = () => {
+        this.props.zoomOut(SCALE_STEP);
+    }
+
     render() {
         return (
             <Toolbar 
                 onChangeBackgroundButtonClick={this.onChangeBackgroundButtonClick}
-                zoomIn={this.props.zoomIn}
-                zoomOut={this.props.zoomOut}
+                zoomIn={this.zoomIn}
+                zoomOut={this.zoomOut}
                 zoomReset={this.props.zoomReset}
                 fileSize={this.getFileSize()}
                 sourceImageValidity={this.props.sourceImageValidity}
